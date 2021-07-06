defe01c19e3d4140484de72e2e476a5e197782fb
Drew-E-Wilson <dwilsrock21@gmail.com>
2021-07-06 12:11:25 -0400

    more sliding window algo complete

--- algorithm_patterns/4_multiple_pointers_pattern.js
+++ algorithm_patterns/4_multiple_pointers_pattern.js
@@ -61,7 +61,7 @@ function numberOfUniqueValues(arr) {
     console.log(i + 1);
     return i + 1;
 }
-numberOfUniqueValues([])
+numberOfUniqueValues([-1, 1, 2, 3, 4, 5])
 
 
 // PROBLEM 3
@@ -128,5 +128,63 @@ function averagePair(arr, target) {
 }
 averagePair([1, 3, 4, 6], 3.5)
 
+// Solution 2
+function averagePairs(arr, num) {
+    let start = 0
+    let end = arr.length - 1;
+    while (start < end) {
+        let avg = (arr[start] + arr[end]) / 2
+        if (avg === num) return true;
+        else if (avg < num) start++
+        else end--
+    }
+    return false;
+}
+
 // PROBLEM 5 
 // Write a functin called isSubsequences which takes in two string and checks whether the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check if the characts in the first string appear somewhere in the second string, without their order changing. 
+function isSubsequence(str1, str2) {
+    let combined = str1 + str2;
+    let word1 = 0;
+    let word2 = word1 + str1.length - 1;
+    while (word2 < combined.length) {
+        if (combined[word1] === combined[word2]) {
+            word1++;
+            word2++;
+            if (word1 === str1.length) {
+                console.log("true");
+                return true;
+            }
+        } else {
+            word2++;
+        }
+    }
+    if (word1 < str1.length) {
+        console.log("false");
+        return false;
+    }
+}
+
+isSubsequence("aba", "abracadabera")
+
+// Solution 2
+function isThisSubsequence(str1, str2) {
+    var i = 0;
+    var j = 0;
+    if (!str1) return true;
+    while (j < str2.length) {
+        if (str2[j] === str1[i]) i++;
+        if (i === str1.length) return true;
+        j++;
+    }
+    return false;
+}
+
+// Solution 3 with recursive
+
+function isSubsequences(str1, str2) {
+    if (str1.length === 0) return true
+    if (str2.length === 0) return false
+    if (str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1))
+    return isSubsequence(str1, str2.slice(1))
+}
--- algorithm_patterns/5_sliding_window_pattern.js
+++ algorithm_patterns/5_sliding_window_pattern.js
@@ -15,12 +15,12 @@ function maxSubarraySum(arr, num) {
     }
     for (let i = 0; i < num; i++) {
         maxSum += arr[i]
-        console.log(maxSum)
+        // console.log(maxSum)
     }
     tempSum = maxSum;
     for (let i = num; i < arr.length; i++) {
         tempSum = tempSum - arr[i - num] + arr[i];
-        console.log(arr[i])
+        // console.log(arr[i])
         maxSum = Math.max(maxSum, tempSum);
     }
     return maxSum;
@@ -44,8 +44,151 @@ const highestSubSum = (arr, num) => {
         tempNum = tempNum - arr[i - num] + arr[i];
         maxNum = Math.max(maxNum, tempNum)
     }
-    console.log(maxNum);
+    // console.log(maxNum);
     return maxNum
 }
 
 highestSubSum([1, 6, 9, 2, 1, 9, 5, 2, 8], 4)
+
+
+// PROBLEM 2
+// Given an array of integers and a number, write a function called maxSubArraySum, which finds the maximum sum of a subarray with the length of the number passed to the function. 
+// Note that a subarray must consist of consecutive elements from the original array. In an example. [100,200,300] is a subarray of [100, 200, 300, 400] but [100, 300] would not be.
+
+function maxSubArraySum(arr, num) {
+    let tempSum = 0;
+    let maxSum = 0;
+    if (arr.length < num) {
+        console.log("null")
+        return null
+    }
+    for (let i = 0; i < num; i++) {
+        maxSum += arr[i]
+    }
+    tempSum = maxSum
+    for (let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i]
+        maxSum = Math.max(tempSum, maxSum)
+    }
+    console.log(maxSum);
+    return maxSum;
+};
+
+maxSubArraySum([100, 200, 300, 400], 2);
+
+// SOLUTION 2 
+function maxSubarraySums(arr, num) {
+    if (arr.length < num) return null;
+
+    let total = 0;
+    for (let i = 0; i < num; i++) {
+        total += arr[i];
+    }
+    let currentTotal = total;
+    for (let i = num; i < arr.length; i++) {
+        currentTotal += arr[i] - arr[i - num];
+        total = Math.max(total, currentTotal);
+    }
+    return total;
+}
+
+
+
+// PROBLEM 3
+// Write a function called minSubArrayLen which accepts 2 parameters - an array of positive integers and a positive integer.
+// This function should return the minimum length of a contiguous subaray of which the sum is greater than or eaqual to the integer passed to the function. If there isn't one, return 0.
+function minSubArrayLen(arr, num) {
+    let total = 0;
+    let length = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (total >= num) {
+            console.log(length)
+            return length
+        } else if (total < num) {
+            total += arr[i];
+            length += 1;
+        }
+    }
+    if (total < num) {
+        console.log("0")
+        return 0
+    }
+    console.log(total)
+}
+minSubArrayLen([2, 3, 2, 4, 3], 7);
+
+// SOLUTION 2 (WORKING FROM DUDE)
+function minSubArrayLenG(nums, sum) {
+    let total = 0;
+    let start = 0;
+    let end = 0;
+    let minLen = Infinity;
+
+    while (start < nums.length) {
+        // if current window doesn't add up to the given sum then 
+        // move the window to right
+        if (total < sum && end < nums.length) {
+            total += nums[end];
+            end++;
+        }
+        // if current window adds up to at least the sum given then
+        // we can shrink the window 
+        else if (total >= sum) {
+            minLen = Math.min(minLen, end - start);
+            total -= nums[start];
+            start++;
+        }
+        // current total less than required total but we reach the end, need this or else we'll be in an infinite loop 
+        else {
+            break;
+        }
+    }
+
+    return minLen === Infinity ? 0 : minLen;
+}
+
+
+// PROBLEM 4
+// Write a function called findLongestSubstring, which accepts a string and returns the length of the longest substring with all distinct characters.
+function findLongestSubstring(str) {
+    let letter1 = 0;
+    let counter = 0;
+    if (str.length === 0) {
+        return 0
+    }
+    for (let i = 0; i < str.length; i++) {
+        console.log(str[i])
+        if (str[letter1] !== str[i + 1]) {
+            counter++;
+            letter1++;
+        } else {
+            console.log(counter + 1);
+            return counter + 1;
+        }
+    }
+    if (counter >= str.length) {
+        console.log(str.length);
+        return str.length
+    }
+}
+findLongestSubstring("thisisawesome")
+
+
+// SOLUTION 2 (WORKING FROM DUDE)
+function findLongestSubstringS(str) {
+    let longest = 0;
+    let seen = {};
+    let start = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (seen[char]) {
+            start = Math.max(start, seen[char]);
+        }
+        // index - beginning of substring + 1 (to include current in count)
+        longest = Math.max(longest, i - start + 1);
+        // store the index of the next char so as to not double count
+        seen[char] = i + 1;
+    }
+    return longest;
+}
